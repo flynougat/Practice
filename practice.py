fc81489ecc5f5314263009a6af62ff2d618fc626
flynougat <chloexma@gmail.com>
2021-09-22 20:11:42 -0700

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,604 @@
+import collections
+from collections import deque
+import heapq
+from os import system
+import sys
+from typing import Counter
+
+class Solution:
+
+    ####################### Binary Search (Array) ###########################
+
+    # 160 Find Minimum in Rotated Sorted Array II
+    # use Binary Search
+    def findMin(self, nums):
+        left, right = 0, len(nums) - 1
+
+        while (left < right):
+            if nums[left] < nums[right]:
+                return nums[left]
+
+            # in Python 3, 6/5 = 1.2, 6//5 = 1
+            mid = left + (right - left) // 2
+            
+            # smallest in left side
+            if nums[left] > nums[mid]:
+                right = mid
+            # smallest in right side
+            elif nums[left] < nums[mid]:
+                left = mid + 1
+            # hard to decide, left + 1
+            else:
+                left += 1
+        return nums[left]
+
+
+
+
+    ####################### Stack ###########################
+
+
+    # 12 · Min Stack
+    # when push to stack, if new < top, new is the new top. if new > top, push top again
+    # O(1) time to return smallest in stack
+    def __init__(self):
+        # do intialization if necessary
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, number):
+        # write your code here
+        self.stack.append(number)
+        if len(self.min_stack)== 0 or number < self.min_stack[len(self.min_stack) - 1]:
+            self.min_stack.append(number)
+        else:
+            self.min_stack.append(self.min_stack[len(self.min_stack) - 1])
+
+    def pop(self):
+        # write your code here
+        self.min_stack.pop()
+        return self.stack.pop()
+
+    def min(self):
+        # only return, not pop. pop will remove element
+        return self.min_stack[len(self.min_stack) - 1]
+
+    
+
+    # 980 · Basic Calculator II
+    # The expression string contains only non-negative integers, +, -, *, / operators 
+    # and empty spaces . 
+    # Division of integers should round off decimals.
+    # 通过栈来实现运算，按顺序读取字符串，将第一个数入栈。之后遇到+，将下一个数num入栈；
+    # 遇到-，则将-num入栈；遇到乘或除，先将上一个数出栈，与当前数进行运算后，再将结果入栈。
+    # 读取完整个字符串后，将栈中所有的数相加即运算结果。
+    def calculate(self, s):
+        if not s:
+            return "0"
+        
+        stack, num, operator = [], 0, '+'
+
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = 10 * num + int(c)
+            if (not c.isdigit() and not c.isspace()) or i == len(s) - 1:
+                if operator == '+':
+                    stack.append(num)
+                elif operator == '-':
+                    stack.append(-num)
+                elif operator == '*':
+                    stack.append(stack.pop() * num)
+                else:
+                    stack.append(int(stack.pop() / num))
+                operator = c
+                num = 0
+        
+        return sum(stack)
+
+
+
+
+####################### String ###########################
+
+    #1889 · Interval Merge (String) 
+    # return true if string intervals can b merged
+    # O(1) time and space
+    def MergeJudge(self, interval_A, interval_B):
+            # get first and last chars from A, B
+            firstChar_A = interval_A.split(interval_A[0])[1].split(',')[0]
+            lastChar_A = interval_A.split(',')[1].split(interval_A[-1])[0]
+            firstChar_B = interval_B.split(interval_B[0])[1].split(',')[0]
+            lastChar_B = interval_B.split(',')[1].split(interval_B[-1])[0]
+
+            if firstChar_A > firstChar_B:
+                if lastChar_B > firstChar_A:
+                    return True
+                if (lastChar_B == firstChar_A) and (interval_A[0] == '[') or (interval_B[-1] == ']'):
+                    return True
+                if ((lastChar_B + "a") == firstChar_A) and (interval_B[-1] == ']') and (interval_A[0] == '['):
+                    return True
+                return False
+            else:
+                if lastChar_A > firstChar_B:
+                    return True
+                if (lastChar_A == firstChar_B) and (interval_A[-1] == ']') or (interval_B[0] == '['):
+                    return True
+                if ((lastChar_A + "a") == firstChar_B) and (interval_A[-1] == ']') and (interval_B[0] == '['):
+                    return True    
+                return False
+
+
+
+    # 418 · Integer to Roman
+    # Given an integer, convert it to a roman numeral.
+    def intToRoman(self, n):
+        digit = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        result = ''
+
+        for d, r in zip(digit, rom):
+            result += r * (n//d)
+            n = n % d
+        return result
+
+
+    # 1263 · Is Subsequence
+    # Given a string s and a string t, check if s is subsequence of t
+    # 直接按s串的顺序去遍历t串
+    def isSubsequence(self, s, t):
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        if i == len(s):
+            return True
+        return False
+
+
+    # 647 · Find All Anagrams in a String
+    # Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+    # If s is an anagram of p, then s is a permutation of p.
+    # sliding window
+    # time O(n), space O(1)
+    def findAnagrams(self, s, p):
+        list = []
+        times = dict()
+        # find how many times each letter occurs in p
+        for c in p:
+            if c not in times:
+                times[c] = 1
+            else:
+                times[c] += 1
+        # create slide window
+        l, r = 0, -1
+        while l < len(s):
+            if r - l + 1 == len(p):
+                list.append(l)
+            if r + 1 < len(s) and s[r+1] in times and times[s[r+1]] > 0:
+                r += 1
+                times[s[r]] -= 1
+            else:
+                if s[l] not in times:
+                    times[s[l]] = 1
+                else:
+                    times[s[l]] += 1
+                l += 1
+        return list
+
+
+    # 1394 · Goat Latin
+    def  toGoatLatin(self, S):
+        vowel = set('aeiouAEIOU')
+        word = []
+        space = ' '
+
+        for i , w in enumerate(S.split()):
+            if w[0] not in vowel:
+                word.append(w[1:] + w[0] + 'ma' + 'a'*(i+1))
+            else:
+                word.append(w + 'ma' + 'a'*(i+1))
+
+        return space.join(word)
+
+
+    # 1721 · Minimum Add to Make Parentheses Valid
+    def minAddToMakeValid(self, S):
+        left = right = 0
+        for i in S:
+            if right == 0 and i == ')':
+                left += 1
+            else:
+                right += 1 if i == '(' else -1
+        return left + right
+
+        
+####################### Matrix ###########################
+
+    # 433 · Number of Islands
+    def numIslands(self, grid):
+        if grid == None or len(grid) == 0:
+            return 0
+
+        result = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    self.islandBFS(grid, i, j)
+                    result += 1
+
+        return result
+
+    def islandBFS(self, grid, x, y):
+        check = [(0,1), (0,-1), (1,0), (-1,0)]
+        queue = deque([(x, y)])
+        
+        while queue:
+            x, y = queue.pop()
+            for dx, dy in check:
+                if 0 <= dx + x < len(grid) and 0 <= dy + y < len(grid[0]) and grid[dx + x][dy + y] == 1:
+                    grid[dx + x][dy + y] = 0
+                    queue.append((dx + x, dy + y))
+
+
+
+
+    # 1272 · Kth Smallest Element in a Sorted Matrix
+    # Given a n x n matrix where each of the rows and columns are sorted in ascending order, 
+    # find the kth smallest element in the matrix.
+    # Matrix + binary search
+    def kthSmallest(self, matrix, k):
+        m = len(matrix)
+        n = len(matrix[0])
+        low, high = matrix[0][0], matrix[m-1][n-1]
+        while low + 1 < high:
+            mid = low + (high - low) // 2
+            count = self.kthSmallestHelper(matrix, mid)
+            if count < k:
+                low = mid
+            else: high = mid
+        if self.kthSmallestHelper(matrix, low) >= k:
+            return low
+        return high
+
+    def kthSmallestHelper(self, matrix, target):
+        m = len(matrix)
+        n = len(matrix[0])
+        i, j = m - 1, 0
+        count = 0
+        while i >=0 and j < n:
+            if matrix[i][j] <= target:
+                count += i +1
+                j += 1
+            else:
+                i -= 1
+        return count
+
+
+
+    # 1205 · Diagonal Traverse
+    # segments = m + n -1
+    # each element on diagonal has the same i+j value. 
+    # i+j odd segment, down left order i++, j--
+    # i+j even segment up right order i-- j++ (reverse)
+    # j organized the order
+    def findDiagonalOrder(self, matrix):
+        if not matrix or not matrix[0]: return []
+
+        m, n = len(matrix), len(matrix[0])
+        segments = [[] for _ in range(m+n-1)]
+
+        for i in range(m):
+            for j in range(n):
+                segments[i+j].append(matrix[i][j])
+
+        result = []
+
+        for x in range(len(segments)):
+            if x % 2 == 1:
+                result.extend(segments[x])
+            else:
+                result.extend(segments[x][::-1])
+        
+        return result
+
+
+    # 1042 · Toeplitz Matrix 
+    # A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+    def isToeplitzMatrix(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        if m == 1 and n == 1: return True
+
+        for i in range(m - 1):
+            for j in range(n - 1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
+
+
+
+####################### Topological Sort ###########################
+    
+    # 616 · Course Schedule II
+
+    def findOrder(self, numCourses, prerequisites):
+        # count indegrees
+        edges = {i : [] for i in range(numCourses)}
+        degrees = [0 for i in range(numCourses)]
+        for i, j in prerequisites:
+            edges[j].append(i)
+            degrees[i] += 1
+        
+        # push 0 indegree to queue
+        queue = []
+        result = []
+        for i in range(numCourses):
+            if degrees[i] == 0:
+                queue.append(i)
+
+        # bfs
+        while queue:
+            course = queue.pop(0)
+            result.append(course)
+            for e in edges[course]:
+                degrees[e] -= 1
+                if degrees[e] == 0:
+                    queue.append(e)
+        if len(result) == numCourses:
+            return result
+        else:
+            return []
+
+
+####################### Array ###########################
+
+    # 606 · Kth Largest Element II
+    #  N is much larger than k
+    # 三种解法：
+    # QuickSelect：平均O(n)，最坏O(n ^ 2)
+    # PriorityQueue：O(nlogk)
+    # Heapify：O(n + klogn)
+    # Because n >> k, heap will be the best solution
+
+    def kthLargestElement2(self, nums, k):
+        if not nums or k < 1:
+            return None
+        # import heapq
+        # 先造一个k - 1数量的minHeap， 再从nums里面k至结尾来update minHeap
+        min_heap = nums[:k]
+        heapq.heapify(min_heap)
+
+        for num in nums[k:]:
+            heapq.heappushpop(min_heap, num)
+        
+        return min_heap[0]
+
+
+
+    # 362 · Sliding Window Maximum
+    # in the While loop
+    # 1. kick out the number smaller than current from queue
+    # 2. put index of current number into queue
+    # 3. start output when there are enough numbers in queue
+    # 4. remove old numbers from front of queue
+    def maxSlidingWindow(self, nums, k):
+
+        queue, result = deque([]), []
+
+        for i in range(len(nums)):
+            while queue and nums[i] >= nums[queue[-1]]:
+                queue.pop()
+            queue.append(i)
+
+            # add num with index queue[0] to result
+            if i + 1 >= k:
+                result.append(nums[queue[0]])
+            # remove index queue[0] from queue
+            if i + 1 - k == queue[0]:
+                queue.popleft()
+        return result
+
+
+
+    # 1212 · Max Consecutive Ones
+    # Given a binary array, find the maximum number of consecutive 1s in this array.
+    def findMaxConsecutiveOnes(self, nums):
+        result = 0
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+                result = max(count, result)
+            else:
+                count = 0
+        return result
+
+
+    # 1310 · Product of Array Except Self
+    # Brute force O(n^2)
+    # 令result为一个全是1的数组。令前缀积prefixProduct等于1，后缀积postfixProduct等于1。
+    # 正序遍历数组nums，第i个位置先将结果乘上prefixProduct，再将prefixProduct乘上nums[i]。
+    # 逆序遍历数组nums，第i个位置先将结果乘上postfixProduct，再将postfixProduct乘上nums[i]。
+    # 返回result。遍历两次数组，时间复杂度为O(N)。只需额外O(1)的时间，记录前缀积和后缀积
+    def productExceptSelf(self, nums):
+        length = len(nums)
+        result = [1] * length # create length size array and put 1 for each element
+        prefix_product = 1
+        postfix_product = 1
+
+        for i in range(length):
+            result[i] *= prefix_product
+            prefix_product *= nums[i]
+
+        for i in range(length - 1, -1, -1):  # range(start, stop, step)
+            result[i] *= postfix_product
+            postfix_product *= nums[i]
+
+        return result
+
+
+    # 1183 · Single Element in a Sorted Array
+    # Input: [1,1,2,3,3,4,4,8,8] Output: 2
+    # use binary search. if nums[mid] != nums[mid + 1], the single is in the left half
+    def singleNonDuplicate(self, nums):
+        n = len(nums)
+        low, high = 0, n-1
+        while low + 1 < high:
+            mid = (high + low) // 2
+            if mid % 2 == 0:
+                if mid + 1 <= n - 1 and nums[mid] != nums[mid + 1]:
+                    high = mid
+                else:
+                    low = mid
+            else:
+                if mid - 1 >= 0 and nums[mid] != nums[mid - 1]:
+                    high = mid
+                else:
+                    low = mid
+
+        if low % 2 == 0:
+            if nums[low] != nums[low + 1] and low + 1 <= n - 1:
+                return nums[low]
+            else:
+                return nums[high]
+        else:
+            if nums[low] != nums[low - 1] and low - 1 >= 0:
+                return nums[low]
+            else:
+                return nums[high]
+
+
+
+    # 838 Subarray sum equals k
+    # find the indices of subarray
+    # prefix sum as key, count as value in a dictionary 当prefix_sum - k 出现在HashMap中，叠加count
+    def subarraySumEqualsK(self, nums, k):
+        count = 0
+        prefixSum = 0
+        dic = {0: 1}
+
+        for num in nums:
+            prefixSum += num
+            if prefixSum - k in dic:
+                count += dic[prefixSum - k]
+            if prefixSum in dic:
+                dic[prefixSum] += 1
+            else:
+                dic[prefixSum] = 1
+        return count
+
+
+
+    # 397 · Longest Continuous Increasing Subsequence
+    # An increasing continuous subsequence:
+    # Can be from right to left or from left to right.
+    # Indices of the integers in the subsequence should be continuous
+    def longestIncreasingContinuousSubsequence(self, A):
+        size = len(A)
+        if size < 1: return 0
+        if size < 2: return 1
+
+        dp1, dp2 = 1, 1
+        globalMax = 0
+
+        for i in range(1, size):
+            dp1 = dp1 + 1 if A[i] > A[i - 1] else 1
+            dp2 = dp2 + 1 if A[i] < A[i - 1] else 1
+            globalMax = max(globalMax, max(dp1, dp2))
+
+        return globalMax
+
+
+####################### Two Pointers ###########################
+
+    # 891 · Valid Palindrome II
+    # Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+    def validPalindrome(self, s):
+        left = 0
+        right = len(s) - 1
+        while left + 1 < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            else:
+                return self.validPalindromeHelper(s, left, right - 1) or self.validPalindromeHelper(s, left + 1, right)
+
+        return True
+    
+    def validPalindromeHelper(self, s, left, right):
+        while left + 1 < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        
+        return True
+
+
+
+
+
+####################### DP ###########################
+
+    # 669 · Coin Change
+    # use the coins values in given array to find min number of coins needed to add up to target
+    # （1）确定状态：dp[i]表达要凑出i元钱可拥有的方案数。所以如果最后一枚硬币面值为c，那么求出dp[i - c]的方案数即可推出dp[i]。
+    # （2）转移方程：假设硬币面值为序列c[n]，dp[i] = min(dp[i - c0] + 1, dp[i - c1] + 1, ... , dp[i - cn] + 1)
+    # （3）初始状态和边界情况：dp[0] = 0，对于凑不出的i值，dp[i]值应该为sys.maxsize
+    # （4）计算顺序：从左往右。
+    # 时间复杂度：O(n * m)，n为目标钱数，m为硬币面值种类数。空间复杂度：O(n)。
+    # import sys
+    def coinChange(self, coins, amount):
+        if not coins or len(coins) == 0 or amount < 0:
+            return -1
+
+        n = len(coins)
+        dp = [sys.maxsize] * (amount + 1) # use amount + 1 to prevent overflow
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for j in range(n):
+                last_index = i - coins[j]
+                if last_index < 0 or dp[last_index] == sys.maxsize:
+                    continue
+                dp[i] = min(dp[i], dp[last_index] + 1)
+
+        if dp[amount] == sys.maxsize:
+            return -1
+        
+        return dp[amount]
+
+
+
+
+
+
+####################### Graph ###########################
+
+    # 1031 · Is Graph Bipartite?
+    # a undirected graph is bipartite if we can split it's set of nodes into two independent subsets A and B 
+    # such that every edge in the graph has one node in A and another node in B.
+    # 二分图染色模板题,通过黑白染色我们可以判断一个无向图是否二分图:
+    # 遍历整个图, 将相邻的节点染成不同的颜色, 如果可以完成这个遍历(即染色过程没有冲突), 说明是二分图.
+    # 可以用BFS或DFS来实现, 只需要根据当前节点的颜色设定下一个节点的颜色即可, 如果下一个节点已经被染成了相同的颜色, 说明发生了冲突.
+    # Use DFS, time O(v+e) space O(v)
+    # 0 means not visited/colored, c means blue, -c means red
+    def isBipartite(self, graph):
+        n = len(graph)
+        self.color = [0] * n
+        for i in range(n):
+            if self.color[i] == 0 and not self.isBipartiteHelper(i, graph, 1):
+                return False
+        return True
+
+    def isBipartiteHelper(self, now, graph, c):
+        self.color[now] = c
+        for nxt in graph[now]:
+            if self.color[nxt] == 0 and not self.isBipartiteHelper(nxt, graph, -c):
+                return False
+            elif self.color[nxt] == self.color[now]:
+                return False
+        return True
---
+++ treeNode.py
@@ -0,0 +1,245 @@
+import collections
+import sys
+
+
+
+####################### Binary Search Tree, BST ###########################
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+    
+
+# 86 · Binary Search Tree Iterator
+# Next() returns the next smallest element in the BST.
+# next() and hasNext() queries run in O(1) time in average.
+# Extra memory usage O(h), h is the height of the tree.
+# Super Star: Extra memory usage O(1)
+#
+# use stack
+class BSTIterator:
+    def __init__(self, root):
+        self.stack = []
+        while root != None:
+            self.stack.append(root)
+            root = root.left
+
+    def hasNext(self):
+        return len(self.stack) > 0
+
+    def _next(self):
+        node = self.stack[-1]
+        if node.right is not None:
+            n = node.right
+            while n != None:
+                self.stack.append(n)
+                n = n.left
+        else:
+            n = self.stack.pop()
+            while self.stack and self.stack[-1].right == n:
+                n = self.stack.pop()
+        return node
+
+
+
+
+# 1311 · Lowest Common Ancestor of a Binary Search Tree
+# use the characteristics of BST,recursively search
+
+class Solution1:
+    def lowestCommonAncestorBST(self, root, p, q):
+        if not root:
+            return
+        # right
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestorBST(root.right, p, q)
+        # left
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestorBST(root.left, p, q)
+        return root
+
+
+
+
+# 910 · Largest BST Subtree
+# subtree contains max number of nodes
+# use a helper, divide and conquer
+# import sys
+    def largestBSTSubtree(self, root):
+        if not root:
+            return 0
+        _, size, _, _ = self.largestBSTSubtreeHelper(root)
+        return size
+
+    def largestBSTSubtreeHelper(self, root):
+        if not root:
+            return True, 0, sys.maxsize, -sys.maxsize
+        
+        l_bst, l_size, l_min, l_max = self.largestBSTSubtreeHelper(root.left)
+        r_bst, r_size, r_min, r_max = self. largestBSTSubtreeHelper(root.right)
+
+        bst = l_bst and r_bst and root.val > l_max and root.val < r_min
+
+        if bst:
+            size = l_size + r_size + 1
+        else:
+            size = max(l_size, r_size)
+        
+        return bst, size, min(l_min, r_min, root.val), max(l_max, r_max, root.val)
+
+
+
+
+
+####################### Binary Tree ###########################
+
+# 760 · Binary Tree Right Side View
+# Given a binary tree, imagine yourself standing on the right side of it, 
+# return the values of the nodes you can see ordered from top to bottom
+#
+# only return right side
+# BFS, 每次queue里最后一个节点就是最右边的.
+class Solution:
+    def rightSideView(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = collections.deque([root])
+
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.popleft()
+                if i == n - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+        return result
+
+
+
+# 88 · Lowest Common Ancestor of a Binary Tree
+# use divide & conquer
+
+    def lowestCommonAncestorBT(self, root, A, B):
+        if not root:
+            return 
+        
+        if root == A or root == B:
+            return root
+        
+        # divide
+        left = self.lowestCommonAncestorBT(root.left, A, B)
+        right = self.lowestCommonAncestorBT(root.right, A, B)
+
+        # conquer
+        # A, B each side
+        if left and right:
+            return root
+        
+        # left side
+        if left:
+            return left
+        
+        # right side
+        if right:
+            return right
+
+        # not at left or right 
+        return None
+
+
+# 468 · Symmetric Binary Tree
+# recursion
+
+    def isSymmetric(self, root):
+        if not root:
+            return True
+        return self.isSymmetricHelper(root.left, root.right)
+
+    def isSymmetricHelper(self, p, q):
+        if p == None and q == None:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSymmetricHelper(p.right, q.left) and self.isSymmetricHelper(p.left, q.right)
+        return False
+
+
+# 7 · Serialize and Deserialize Binary Tree
+# use bfs to traverse each level
+# record node val in 
+    def serialize(self, root):
+        if not root:
+            return '{}'
+        
+        queue = collections.deque()
+        queue.append(root)
+        result = ''
+        while queue:
+            node = queue.popleft()
+            if node:
+                result += str(node.val) + ','
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                result += '#,'
+        result = "{" + result[:-1] + "}"
+        return result
+
+
+    def deserialize(self, data):
+        if not data or data == '{}':
+            return None
+
+        items = data[1:-1].split(',')
+        index = 0
+        root = TreeNode(items[index])
+
+        queue = collections.deque()
+        queue.append(root)
+        index += 1
+
+        while index < len(items):
+            node = queue.popleft()
+            # left first
+            if items[index] != '#':
+                node.left = TreeNode(items[index])
+                queue.append(node.left)
+                index += 1
+            
+            # right
+            if items[index] != '#':
+                node.right = TreeNode(items[index])
+                queue.append(node.right)
+                index += 1
+
+        return root
+            
+# 1115 · Average of Levels in Binary Tree
+    def averageOfLevels(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = collections.deque([root])
+
+        while queue:
+            n = len(queue)
+            sum_level = 0
+            for i in range(n):
+                node = queue.popleft()
+                sum_level += node.val
+                if i == n - 1:
+                    result.append(sum_level / n)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+        return result
